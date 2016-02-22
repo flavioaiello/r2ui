1ae08203ef32682b5f074e4f51cd6ade89cfa6a9
Flavio Aiello <flavio.aiello@swisscom.com>
2016-02-22 11:27:53 +0100

    refactoring

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -e
 
 # Slave container to execute the build
-docker build -t 'node-slave' -f components/node-slave/Dockerfile components/node-slave/
+docker build -t 'node-slave' -f build/Dockerfile build
 docker run --rm \
     -v $PWD/build:/app \
-    -v $PWD/components/r2ui/src/usr/share/nginx/html/:/app/dist \
+    -v $PWD/src/usr/share/nginx/html/:/app/dist \
     -w /app \
     -t node-slave \
     /bin/sh -c "npm install && \
